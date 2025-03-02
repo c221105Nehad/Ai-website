64479d7b66ed2ed963e8ec1de45ed4505b9d6bc0
c221105Nehad <nehadinkiadhossain@gmail.com>
2025-03-02 19:45:13 +0600

    Pricing Section Completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Hero from "./components/Hero";
 import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
 import Services from "./components/Services";
+import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
 const App = () => {
   return (
     <>
@@ -13,6 +15,8 @@ const App = () => {
         <Benefits />
         <Collaboration />
         <Services />
+        <Pricing />
+        <Roadmap />
       </div>
 
       <ButtonGradient />
---
+++ src/components/Roadmap.jsx
@@ -0,0 +1,14 @@
+import { Section } from "./Section";
+import Heading from "./Heading";
+
+const Roadmap = () => {
+  return (
+    <Section className="overflow-hidden" id="roadmap">
+        <div>
+            <Heading />
+        </div>
+    </Section>
+  )
+}
+
+export default Roadmap
